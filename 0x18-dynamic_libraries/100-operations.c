bb75593108034f38b6ebec6423b86dd5d5aa1620
Asmae2001 <himdiasmaebr@gmail.com>
2023-12-15 13:39:11 -0800

    task

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,39 @@
+// 100-operations.c
+
+#include <stdio.h>
+
+// Function to add two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Function to subtract two integers
+int sub(int a, int b) {
+    return a - b;
+}
+
+// Function to multiply two integers
+int mul(int a, int b) {
+    return a * b;
+}
+
+// Function to divide two integers
+int div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        fprintf(stderr, "Error: Division by zero\n");
+        return 0;
+    }
+}
+
+// Function to calculate the remainder of the division
+int mod(int a, int b) {
+    if (b != 0) {
+        return a % b;
+    } else {
+        fprintf(stderr, "Error: Modulo by zero\n");
+        return 0;
+    }
+}
+
